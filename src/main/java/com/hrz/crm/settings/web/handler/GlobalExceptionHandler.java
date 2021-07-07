7cdd8db61e782bf7d4625ef56a15b67087f5a64a
黄瑞召 <Horizon@152.com>
2021-07-07 09:19:29 +0800

    settings web handler(全局异常处理)

---
+++ src/main/java/com/hrz/crm/settings/web/handler/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.hrz.crm.settings.web.handler;
+
+import com.hrz.crm.settings.web.Exception.*;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author : Horizon~muu
+ * @Date: 2021/05/10/20:05
+ * @Description:
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = UserLoginInforException.class)
+    public Map doUserExistException(){
+        Map map = new HashMap();
+        map.put("success",false);
+        map.put("msg","用户不存在！！！请核对登录信息");
+        return map;
+    }
+    @ResponseBody
+    @ExceptionHandler(value = UserExpireTimeException.class)
+    public Map doUserExpireTimeException(){
+        Map map = new HashMap();
+        map.put("success",false);
+        map.put("msg","账户已过期，请联系管理员~~~");
+        return map;
+    }
+    @ResponseBody
+    @ExceptionHandler(value = UserLockStateException.class)
+    public Map doUserLockStateException(){
+        Map map = new HashMap();
+        map.put("success",false);
+        map.put("msg","账户已锁定，请联系管理员~~~");
+        return map;
+    }
+    @ResponseBody
+    @ExceptionHandler(value = UserIpException.class)
+    public Map doUseIpException(){
+        Map map = new HashMap();
+        map.put("success",false);
+        map.put("msg","非法IP，请联系管理员~~");
+        return map;
+    }
+
+    @ExceptionHandler(value = NoLoginException.class)
+    public String doNoLoginException(HttpServletRequest request, HttpServletResponse response){
+        return "/login.jsp";
+    }
+
+}
